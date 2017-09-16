LineageOS/android_device_oneplus_oneplus2

oneplus2: update init
Change-Id: Id87bfa08bcecda40f9bc38d04b799298ce117fa0

init/init_oneplus2.cpp
@@ -36,10 +36,11 @@
 
 void init_variant_properties() {
 
-    std::string device = property_get("ro.cm.device");
+    std::string device;
     std::string rf_version;
 
-    if (device != "oneplus2")
+    platform = property_get("ro.board.platform");
+    if (platform != ANDROID_TARGET)
         return;
 
     rf_version = property_get("ro.boot.rf_v1");

hardwareadictos/validus_device_oneplus_oneplus2

init/init_oneplus2.cpp
@@ -36,10 +36,11 @@
 
 void init_variant_properties() {
 
-    std::string device = property_get("ro.product.device");
+    std::string platform;
     std::string rf_version;
 
-    if (device != "OnePlus2")
+    platform = property_get("ro.board.platform");
+    if (platform != ANDROID_TARGET)
         return;
 
     rf_version = property_get("ro.boot.rf_v1");